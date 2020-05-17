7850f0bc35f3355514ef612b4f62beb2912e94f6
buzzz321 <anders.olme@gmail.com>
2020-05-17 19:57:49 +0200

    Support for objects
    initial support for objects, still not support for nested objects
    or arrays.

--- myjson.py
+++ myjson.py
@@ -1,5 +1,46 @@
 #!/usr/bin/env python3
 
+from enum import Enum
+
+
+class JType(Enum):
+    JString = 1
+    JNumber = 2
+    JObject = 3
+    JArray = 4
+    Undef = 0
+
+
+class JValue:
+    def __init__(self, type=JType.Undef, value=None):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return "[%s, %s]" % (self.type, self.value)
+
+    def __repr__(self):
+        return "[%s, %s]" % (self.type, self.value)
+
+    def getValue(self):
+        return self.value
+
+    def getType(self):
+        return self.type
+
+
+class JSObject:
+    def __init__(self, key=None, value=None):
+        self.data = {}
+        if key is not None:
+            self.data[key] = value
+
+    def __str__(self):
+        return "%s" % (self.data)
+
+    def add(self, key, value):
+        self.data[key] = value
+
 
 class Parser:
     def __init__(self, data):
@@ -7,22 +48,27 @@ class Parser:
         self.currPos = 0
 
     def peek(self):
-        return self.data[self.currPos]
+        if self.currPos < len(self.data):
+            return self.data[self.currPos]
+        return ""
 
     def peekNext(self):
-        if len(data) < self.currPos + 1:
+        if self.currPos + 1 < len(self.data):
             return self.data[self.currPos + 1]
         return ""
 
     def consume(self, char):
-        if self.data[self.currPos] == char:
+        if (self.currPos < len(self.data)) and (self.data[self.currPos] == char):
             self.currPos += 1
             return True
         return False
 
     def consumeWhiteSpace(self):
         length = len(self.data)
-        while self.data[self.currPos].isspace():
+
+        while self.currPos < length:
+            if not self.data[self.currPos].isspace():
+                return True
             self.currPos += 1
             if self.currPos >= length:
                 self.currPos -= 1
@@ -34,17 +80,63 @@ class Parser:
         if not self.consume('"'):
             return ""  # start not found so return empty string
         start = self.currPos
-        while self.currPos < length:
-            if self.data[self.currPos] == '"':
-                break
+        while self.currPos < length and self.data[self.currPos] != '"':
             self.currPos += 1
-        return self.data[start : self.currPos]
+
+        self.consume('"')
+        return self.data[start : self.currPos - 1]
 
     def parseNumber(self):
         length = len(self.data)
         start = self.currPos
         while self.currPos < length:
-            if not self.data[self.currPos].isdigit and (not (self.data[self.currPos] == ".")) and (not (self.data[self.currPos]== "-")):
+            if self.data[self.currPos].isdigit():
+                self.currPos += 1
+            elif (self.data[self.currPos] == ".") or (self.data[self.currPos] == "-"):
+                self.currPos += 1
+            else:
                 break
-            self.currPos += 1
+
         return self.data[start : self.currPos]
+
+    def parseObject(self):
+        length = len(self.data)
+        retVal = JSObject()
+        key = ""
+        value = JSObject("", JValue(0, ""))
+        self.consume("{")
+
+        while True:
+            self.consumeWhiteSpace()
+            if self.consume("}"):
+                return retVal
+
+            key = self.parseQuotedString()
+            self.consumeWhiteSpace()
+            self.consume(":")
+            value = self.parseValue()
+            retVal.add(key, value)
+            # print(retVal)
+            self.consumeWhiteSpace()
+            self.consume(",")
+
+    def parseValue(self):
+        self.consumeWhiteSpace()
+
+        ch = self.peek()
+
+        if ch == "{":
+            obj = self.parseObject()
+            return JValue(JType.JObject, obj)
+        elif ch == "[":
+            arr = self.parseArray()
+            return JValue(JType.JArray, arr)
+        elif ch.isdigit():
+            number = self.parseNumber()
+            return JValue(JType.JNumber, number)
+        elif ch == '"':
+            strVal = self.parseQuotedString()
+            return JValue(JType.JString, strVal)
+        else:
+            print("ch=" + str(ch) + "|")
+            raise Exception("Unknown value type. ")
--- myjson_test.py
+++ myjson_test.py
@@ -1,12 +1,12 @@
 import unittest
 from myjson import *
 
-class TestMyJsonMethods(unittest.TestCase):
 
+class TestMyJsonMethods(unittest.TestCase):
     def test_comsumeWhiteSpace(self):
         uat = Parser("   \n\t  werp")
         uat.consumeWhiteSpace()
-        self.assertEqual("werp", uat.data[uat.currPos:])
+        self.assertEqual("werp", uat.data[uat.currPos :])
 
     def test_parseQuotedString(self):
         uat = Parser('"key":')
@@ -14,21 +14,45 @@ class TestMyJsonMethods(unittest.TestCase):
         self.assertEqual("key", result)
 
     def test_parseNumber(self):
-        uat = Parser('123')
+        uat = Parser("123")
         result = uat.parseNumber()
         self.assertEqual("123", result)
 
     def test_parseFractionalNumber(self):
-        uat = Parser('123.4')
+        uat = Parser("123.4")
         result = uat.parseNumber()
         self.assertEqual("123.4", result)
 
     def test_parseNegativeFractionalNumber(self):
-        uat = Parser('-123.4')
+        uat = Parser("-123.4")
         result = uat.parseNumber()
         self.assertEqual("-123.4", result)
 
+    def test_parseSimpleObject(self):
+        uat = Parser('{ "nyckel" : "värde" }')
+        result = uat.parseObject()
+
+        self.assertEqual("nyckel", list(result.data)[0])
+        self.assertEqual("värde", result.data["nyckel"].getValue())
+        self.assertEqual(JType.JString, result.data["nyckel"].getType())
+
+    def test_parseMultiElementObject(self):
+        uat = Parser('{ "red" : 1, "green" : 2, "yellow": 3 }')
+        result = uat.parseObject()
+
+        print(result)
+        self.assertEqual("red", list(result.data)[0])
+        self.assertEqual("1", result.data["red"].getValue())
+        self.assertEqual(JType.JNumber, result.data["red"].getType())
+
+        self.assertEqual("green", list(result.data)[1])
+        self.assertEqual("2", result.data["green"].getValue())
+        self.assertEqual(JType.JNumber, result.data["green"].getType())
+
+        self.assertEqual("yellow", list(result.data)[2])
+        self.assertEqual("3", result.data["yellow"].getValue())
+        self.assertEqual(JType.JNumber, result.data["yellow"].getType())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
